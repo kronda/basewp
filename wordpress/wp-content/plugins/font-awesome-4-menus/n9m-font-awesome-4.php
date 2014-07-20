8dfaddadd0c3b24f642ad43a1a1ca4da71a25c8a
Kronda <ephany@gmail.com>
2014-07-19 17:19:09 -0700

    replaced font awesome with font awesome for menus

---
+++ wordpress/wp-content/plugins/font-awesome-4-menus/n9m-font-awesome-4.php
@@ -0,0 +1,248 @@
+<?php
+/*
+Plugin Name: Font Awesome 4 Menus
+Plugin URI: http://www.newnine.com/plugins/font-awesome-4-menus
+Description: Join the retina/responsive revolution by easily adding Font Awesome 4.1 icons to your WordPress menus and anywhere else on your site! No programming necessary.
+Version: 4.1.0.1
+Author: New Nine Media
+Author URI: http://www.newnine.com
+License: GPLv2 or later
+*/
+
+/*
+    Copyright 2013  NEW NINE MEDIA, L.P.  (tel : +1-800-288-9699)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class FontAwesomeFour {
+
+    var $defaults;
+
+    function admin_enqueue_scripts( $hook ){
+        if( 'settings_page_n9m-font-awesome-4-menus' == $hook ){
+            wp_enqueue_script( 'n9m-admin-font-awesome-4', plugins_url( 'js/admin.js', __FILE__ ), array( 'jquery' ), $this->defaults[ 'version' ] );
+            wp_enqueue_style( 'n9m-admin-font-awesome-4', plugins_url( 'css/font-awesome.min.css', __FILE__ ), false, $this->defaults[ 'version' ] );
+        }
+    }
+
+    function admin_menu(){
+        add_submenu_page( 'options-general.php', 'Font Awesome 4 Menus', 'Font Awesome', 'edit_theme_options', 'n9m-font-awesome-4-menus', array( $this, 'admin_menu_cb' ) );
+    }
+
+    function admin_menu_cb(){
+        if( $_POST && check_admin_referer( 'n9m-fa' ) ){
+            $settings = array();
+            switch( $_POST[ 'n9m_location' ] ){
+                case 'local':
+                case 'maxcdn':
+                case 'none':
+                    $settings[ 'stylesheet' ] = $_POST[ 'n9m_location' ];
+                    break;
+                case 'other':
+                    $settings[ 'stylesheet' ] = 'other';
+                    $settings[ 'stylesheet_location' ] = sanitize_text_field( $_POST[ 'n9m_location-other-location' ] );
+                    break;
+            }
+            if( isset( $_POST[ 'n9m_text_spacing' ] ) ){
+                $settings[ 'spacing' ] = 1;
+            } else {
+                $settings[ 'spacing' ] = 0;
+            }
+            update_option( 'n9m-font-awesome-4-menus', $settings );
+            print '<div class="updated"><p>Your settings have been saved!</p></div>';
+        }
+        $settings = get_option( 'n9m-font-awesome-4-menus', $this->defaults );
+        print ' <div class="wrap">
+                    <h2><i class="fa fa-thumbs-o-up"></i> '.get_admin_page_title().'</h2>
+                    <p>Thank you for using Font Awesome 4 Menus by <a href="http://www.newnine.com" target="_blank">New Nine</a>! To view available icons, <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">click here to visit the Font Awesome website</a>.</p>
+                    <form action="'.admin_url( 'options-general.php?page=n9m-font-awesome-4-menus' ).'" method="post">
+                        <h3>Font Awesome Stylesheet</h3>
+                        <p>Select how you want Font Awesome 4&#8217;s stylesheet loaded on your site (if at all):</p>
+                        <table class="form-table">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Load Font Awesome 4 From:</th>
+                                    <td>
+                                        <fieldset>
+                                            <legend class="screen-reader-text"><span>Load Font Awesome 4 From</span></legend>
+                                            <label for="n9m_location-local"><input type="radio" name="n9m_location" id="n9m_location-local" value="local"'.( 'local' == $settings[ 'stylesheet' ] ? ' checked' : false ).'> Local plugin folder (default)</label>
+                                            <br />
+                                            <label for="n9m_location-maxcdn"><input type="radio" name="n9m_location" id="n9m_location-maxcdn" value="maxcdn"'.( 'maxcdn' == $settings[ 'stylesheet' ] ? ' checked' : false ).'> Official Font Awesome CDN <span class="description">(<a href="http://www.bootstrapcdn.com/#fontawesome_tab" target="_blank">Bootstrap CDN powered by MaxCDN</a>)</span></label>
+                                            <br />
+                                            <label for="n9m_location-other"><input type="radio" name="n9m_location" id="n9m_location-other" value="other"'.( 'other' == $settings[ 'stylesheet' ] ? ' checked' : false ).'> A custom location:</label> <input type="text" name="n9m_location-other-location" id="n9m_location-other-location" placeholder="Enter full url here" class="regular-text" value="'.( isset( $settings[ 'stylesheet_location' ] ) ? $settings[ 'stylesheet_location' ] : '' ).'">
+                                            <br />
+                                            <label for="n9m_location-none"><input type="radio" name="n9m_location" id="n9m_location-none" value="none"'.( 'none' == $settings[ 'stylesheet' ] ? ' checked' : false ).'>Don&#8217;t load Font Awesome 4&#8217;s stylesheet <span class="description">(use this if you load Font Awesome 4 elsewhere on your site)</span></label>
+                                        </fieldset>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h3>Icon Spacing</h3>
+                        <p>By default, Font Awesome 4 Menus adds a space before or after the icon in your menu. Uncheck the box below to remove this space and give you finer control over your custom styling.</p>
+                        <p><label for="n9m_text_spacing"><input type="checkbox" name="n9m_text_spacing" id="n9m_text_spacing" value="1"'.( 1 == $settings[ 'spacing' ] ? ' checked' : false ).'> Keep the space between my text and my icons <span class="description">(default is checked)</span></label>
+                        <p>'.wp_nonce_field( 'n9m-fa' ).'<button type="submit" class="button button-primary">Save Settings</button></p>
+                    </form>
+                </div>';
+    }
+
+    function admin_notice(){
+        global $current_user, $pagenow;
+        if( isset( $_REQUEST[ 'action' ] ) && 'kill-n9m-font-awesome-4-notice' == $_REQUEST[ 'action' ] ){
+            update_user_meta( $current_user->data->ID, 'n9m-font-awesome-4-notice-hide', 1 );
+        }
+        $shownotice = get_user_meta( $current_user->data->ID, 'n9m-font-awesome-4-notice-hide', true );
+        if( 'plugins.php' == $pagenow && !$shownotice ){
+            print ' <div class="updated">
+                        <div style="float: right;"><a href="?action=kill-n9m-font-awesome-4-notice" style="color: #7ad03a; display: block; padding: 8px;">&#10008;</a></div>
+                        <p>Thank you for installing Font Awesome Menus 4 by <a href="http://www.newnine.com">New Nine</a>! Want to see what else we&#8217;re up to? Subscribe below to our infrequent updates. You can unsubscribe at any time.</p>
+                        <form action="http://newnine.us2.list-manage.com/subscribe/post?u=067bab5a6984981f003cf003d&amp;id=1b25a2aee6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+                            <p><input type="text" name="FNAME" placeholder="First Name" value="'.( !empty( $current_user->first_name ) ? $current_user->first_name : '' ).'"> <input type="text" name="LNAME" placeholder="Last Name" value="'.( !empty( $current_user->last_name ) ? $current_user->last_name : '' ).'"> <input type="text" name="EMAIL" placeholder="Email address" required value="'.$current_user->user_email.'"> <input type="hidden" id="group_1" name="group[14489][1]" value="1"> <input type="submit" name="subscribe" value="Join" class="button action"></p>
+                        </form>
+                    </div>';
+        }
+    }
+
+    function menu( $nav ){
+        $menu_item = preg_replace_callback(
+            '/(<li[^>]+class=")([^"]+)("?[^>]+>[^>]+>)([^<]+)<\/a>/',
+            array( $this, 'replace' ),
+            $nav
+        );
+        return $menu_item;
+    }
+    
+    function replace( $a ){
+        $start = $a[ 1 ];
+        $classes = $a[ 2 ];
+        $rest = $a[ 3 ];
+        $text = $a[ 4 ];
+        $before = true;
+        
+        $class_array = explode( ' ', $classes );
+        $fontawesome_classes = array();
+        foreach( $class_array as $key => $val ){
+            if( 'fa' == substr( $val, 0, 2 ) ){
+                if( 'fa' == $val ){
+                    unset( $class_array[ $key ] );
+                } elseif( 'fa-after' == $val ){
+                    $before = false;
+                    unset( $class_array[ $key ] );
+                } else {
+                    $fontawesome_classes[] = $val;
+                    unset( $class_array[ $key ] );
+                }
+            }
+        }
+        
+        if( !empty( $fontawesome_classes ) ){
+            $fontawesome_classes[] = 'fa';
+            $settings = get_option( 'n9m-font-awesome-4-menus', $this->defaults );
+            if( $before ){
+                if( 1 == $settings[ 'spacing' ] ){
+                    $text = ' '.$text;
+                }
+                $newtext = '<i class="'.implode( ' ', $fontawesome_classes ).'"></i><span class="fontawesome-text">'.$text.'</span>';
+            } else {
+                if( 1 == $settings[ 'spacing' ] ){
+                    $text = $text.' ';
+                }
+                $newtext = '<span class="fontawesome-text">'.$text.'</span><i class="'.implode( ' ', $fontawesome_classes ).'"></i>';
+            }
+        } else {
+            $newtext = $text;
+        }
+        
+        $item = $start.implode( ' ', $class_array ).$rest.$newtext.'</a>';
+        return $item;
+    }
+    
+    function shortcode_icon( $atts ){
+        extract( shortcode_atts( array(
+            'class' => '',
+        ), $atts ) );
+        if( !empty( $class ) ){
+            $fa_exists = false;
+            $class_array = explode( ' ', $class );
+            foreach( $class_array as $c ){
+                if( 'fa' == $c ){
+                    $fa_exists = true;
+                }
+            }
+            if( !$fa_exists ){
+                array_unshift( $class_array, 'fa' );
+            }
+            return '<i class="'.implode( ' ', $class_array ).'"></i>';
+        }
+    }
+    
+    function shortcode_stack( $atts, $content = null ){
+        extract( shortcode_atts( array(
+            'class' => '',
+        ), $atts ) );
+        if( empty( $class ) ){
+            $class_array = array( 'fa-stack' );
+        } else {
+            $fa_stack_exists = false;
+            $class_array = explode( ' ', $class );
+            foreach( $class_array as $c ){
+                if( 'fa-stack' == $c ){
+                    $fa_stack_exists = true;
+                }
+            }
+            if( !$fa_stack_exists ){
+                array_unshift( $class_array, 'fa-stack' );
+            }
+        }
+        return '<span class="'.implode( ' ', $class_array ).'">'.do_shortcode( $content ).'</span>';
+    }
+
+    function wp_enqueue_scripts(){
+        $settings = get_option( 'n9m-font-awesome-4-menus', $this->defaults );
+        switch( $settings[ 'stylesheet' ] ){
+            case 'local':
+                wp_register_style( 'font-awesome-four', plugins_url( 'css/font-awesome.min.css', __FILE__ ), array(), $this->defaults[ 'version' ], 'all' );
+                wp_enqueue_style( 'font-awesome-four' );
+                break;
+            case 'maxcdn':
+                wp_register_style( 'font-awesome-four', $this->defaults[ 'maxcdn_location' ], array(), $this->defaults[ 'version' ], 'all' );
+                wp_enqueue_style( 'font-awesome-four' );
+                break;
+            case 'none':
+                break;
+            case 'other':
+                wp_register_style( 'font-awesome-four', $settings[ 'stylesheet_location' ], array(), $this->defaults[ 'version' ], 'all' );
+                wp_enqueue_style( 'font-awesome-four' );
+                break;
+        }
+    }
+    
+    
+    function __construct(){
+        $this->defaults = array(
+            'maxcdn_location' => '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css',
+            'spacing' => 1,
+            'stylesheet' => 'local',
+            'version' => '4.1.0'
+        );
+        add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+        add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+        add_action( 'admin_notices', array( $this, 'admin_notice' ) );
+        add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+        add_filter( 'wp_nav_menu' , array( $this, 'menu' ), 10, 2 );
+        add_shortcode( 'fa', array( $this, 'shortcode_icon' ) );
+        add_shortcode( 'fa-stack', array( $this, 'shortcode_stack' ) );
+    }
+}
+$n9m_font_awesome_four = new FontAwesomeFour();
---
+++ wordpress/wp-content/plugins/font-awesome-4-menus/readme.txt
@@ -0,0 +1,95 @@
+=== Font Awesome 4 Menus ===
+Contributors: New Nine
+Author URI: http://www.newnine.com
+Tags: menus, font awesome, navigation, responsive, nav menu, wp_nav_menu
+Requires at least: 3
+Tested up to: 3.9.1
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows you to add Font Awesome 4 icons to your WordPress menus or anywhere on your site. No programming necessary!
+
+== Description ==
+
+Add Font Awesome 4 icons to your WordPress menus and site without touching a single line of code! With this plugin, just add fa-(icon name) as a class/classes to your menu and the plugin will pull that out, put the icon before or after your link text, and wrap your link text in a span so you can show or hide it as you see fit.
+
+In Font Awesome 4 Menus, we have added the ability to add multiple Font Awesome classes for multiple effects (eg, fa-home fa-fw fa-2x will add a fixed-width home icon at two times the normal size).
+
+In addition, we have given you shortcodes to add icons to your posts and pages, as well as shortcodes to take advantage of the new stacked feature of Font Awesome 4.
+
+== Installation ==
+
+Use the WordPress installer; or, download and unzip the files and put the folder `n9m-font-awesome-4` into the `/wp-content/plugins/` directory.
+
+Then, activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= Where can I find all of the available icons? =
+
+Head over to [the Font Awesome website](http://fortawesome.github.io/Font-Awesome/icons "the Font Awesome website") to find a full list of icons available. Don't forget to check out the [examples page](http://fortawesome.github.io/Font-Awesome/examples "examples page") to see how you can mix and match icons for new effects.
+
+= How to I add an icon to my menu? =
+
+Go to Appearance -> Menus, select which menu item to which you want to add the icon, and add the icon class(es) under 'CSS Classes (optional)'. (eg, to add the home icon to your 'Home' link, enter "fa-home" (without quotes) as a class. To make it spin, add "fa-home fa-spin" as your classes.) Save your menu and voila!
+
+Want to add an icon to a post or page? Use the shortcodes available:
+
+* Add a single home icon: [fa class="fa-home"]
+* Add a stacked Twitter icon: [fa-stack][fa class="fa-square-o fa-stack-2x"][fa class="fa-twitter fa-stack-1x"][/fa-stack]
+
+You can also use [any of the Font Awesome icons and options](http://fortawesome.github.io/Font-Awesome "any of the Font Awesome icons and options") with the i class in your theme:
+
+* Add a home icon: `<i class="fa fa-home"></i>`
+* Add a home icon: `<i class="fa fa-home"></i>`
+
+= How to I place the icon after my menu text? =
+
+Add "fa-after" (excluding the quotes) as one of your classes and the icon will show up after your text.
+
+= Why don't I see an option to add classes? =
+
+Under Appearance -> Menus, click 'Screen Options' (top right of screen) and make sure that 'CSS Classes' is checked. If not - check it!
+
+= Can I hide the text and just show the icons for my menu? =
+
+Yes. Font Awesome menus adds a space between the icon and the text, and wraps that portion in a span with a class of "fontawesome-text". To hide the text and just show the icon, you can put `.fontawesome-text {display: none;}` in your stylesheet.
+
+We have also added an administrative option under Settings->Font Awesome so you can delete the space before or after the text and really take full control of your style and layout.
+
+= Can I use a CDN? Or, my theme uses Font Awesome. Can I prevent it from loading twice? =
+
+Yes! Go to Settings->Font Awesome and choose how you want Font Awesome 4 loaded. You can use the default (locally from the plugin), you can use the official MaxCDN version, you can set your own url or CDN path, or you can tell the plugin NOT to load the stylesheet at all but still enjoy the Font Awesome Menus.
+
+= Will this bloat or slow down my WordPress? =
+
+No. On your site, Font Awesome 4 Menus will load one of four ways depending on how you set it up:
+
+* loads one minified stylesheet (18kb) to load the fonts, or
+* loads the fonts from the official Font Awesome 4 CDN at MaxCDN, or
+* loads the fonts from a custom url or CDN you choose, or
+* doesn't load the fonts at all if you choose this option.
+
+We use it on mobile-first responsive sites (and our own site) all the time without any noticeable performance drag.
+
+= What happens to my menus if I deactivate/uninstall this? =
+
+Your site will be fine. Where you used Font Awesome 4 Menus, those menu items will just have additional classes (fa-whatever) that you can erase or ignore (or style differently).
+
+If you uninstall the plugin, Font Awesome 4 Menus cleans up after itself by removing the site option and user meta that it set in your database. No bloat here and we clean up after ourselves!
+
+== Upgrade Notice ==
+
+= 4.1.0.1 =
+This upgrade allows you to load Font Awesome from a CDN or prevent double-loading of the stylesheet. It also adds the ability to remove the space in your menus for finer style control.
+
+== Changelog ==
+
+= 4.1.0.1 =
+* Added MaxCDN as an optional stylesheet source
+* Added the ability to set your own Font Awesome stylesheet url
+* Added the ability to prevent Font Awesome 4 Menus from loading the stylesheet if your theme or another plugin already does
+* Added an administration screen
+* Added an uninstall function to clean up after itself upon uninstalling the plugin
+* Updated the readme to reflect the changes in this update
