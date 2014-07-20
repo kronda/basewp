d0370a74110e67156d7cd06e2ac02092011feffd
Kronda <ephany@gmail.com>
2014-07-19 17:34:33 -0700

    widget context

---
+++ wordpress/wp-content/plugins/widget-context/js/widget-context.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) {
+
+	function show_hide_controls( widget_id ) {
+
+		var value = $( '#widget-context-' + widget_id + ' .wc-field-select-condition select' ).val();
+
+		if ( value == 'show' || value == 'hide' ) {
+			$( '#widget-context-' + widget_id ).addClass('context-global');
+		} else {
+			$( '#widget-context-' + widget_id ).removeClass('context-global');
+		}
+
+	}
+
+	$('.widget-context-inside').each(function() {
+
+		show_hide_controls( $(this).data('widget-id') );
+
+	});
+
+	$('#widgets-right, #widgets-left, #customize-theme-controls').on( 'change', '.wc-field-select-condition select', function(){
+		
+		show_hide_controls( $(this).parent().data('widget-id') );
+
+	});
+
+});
